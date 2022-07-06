25a7ca8c2f80ddc5f0542644de4544e1e50f026f
Aman-Mandal <002amankumar@gmail.com>
2022-07-06 16:20:44 +0530

    Added validations

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,15 +1,59 @@
-const SimpleInput = (props) => {
+import { useRef, useState } from 'react'
+
+const SimpleInput = props => {
+  // const inputRef = useRef()
+  const [enteredInput, setEnteredInput] = useState('')
+  const [enteredInputIsValid, setEnteredInputIsValid] = useState(false)
+  const [enteredInputIsTouched, setEnteredInputIsTouched] = useState(false)
+
+  // using State
+  const inputChangeHandler = event => {
+    setEnteredInput(event.target.value)
+  }
+
+  const formSubmitHandler = event => {
+    event.preventDefault()
+    setEnteredInputIsTouched(true)
+
+    if (enteredInput.trim() === '') {
+      setEnteredInputIsValid(false)
+      return
+    }
+
+    // console.log(inputRef.current.value)
+    console.log(enteredInput)
+    setEnteredInputIsValid(true)
+
+    // inputRef.current.value = '' ==> NOT IDEAL
+    setEnteredInput('')
+  }
+
+  const enteredInputIsInvalid = !enteredInputIsValid && enteredInputIsTouched
+
+  const customClasses = enteredInputIsInvalid
+    ? 'form-control invalid'
+    : 'form-control'
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmitHandler}>
+      <div className={customClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          // ref={inputRef}
+          type="text"
+          id="name"
+          onChange={inputChangeHandler}
+          value={enteredInput}
+        />
+        {enteredInputIsInvalid && (
+          <p className="error-text">Name can't be empty.</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
       </div>
     </form>
-  );
-};
+  )
+}
 
-export default SimpleInput;
+export default SimpleInput
